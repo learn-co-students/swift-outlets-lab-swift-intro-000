7e3278d977e39c3d9fba00c4bb80048aaae3e45b
pacosta613 <pedro.acosta613@gmail.com>
2016-11-07 11:27:19 -0500

    Done. It only works once.

--- SimonSaysLab/ViewController.swift
+++ SimonSaysLab/ViewController.swift
@@ -13,12 +13,59 @@ class ViewController: UIViewController {
     @IBOutlet weak var displayColorView: UIView!
     @IBOutlet weak var startGameButton: UIButton!
     @IBOutlet weak var winLabel: UILabel!
+    @IBOutlet weak var redButton: UIButton!
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var greenButton: UIButton!
+    @IBOutlet weak var yellowButton: UIButton!
     var simonSaysGame = SimonSays()
     var buttonsClicked = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        winLabel.isHidden = true
     }
+
+    @IBAction func clickRed(_ sender: UIButton) {
+        simonSaysGame.guessRed()
+        
+        buttonsClicked += 1
+        results()
+    }
+    
+    @IBAction func clickGreen(_ sender: UIButton) {
+        simonSaysGame.guessGreen()
+        
+        buttonsClicked += 1
+        results()
+    }
+    
+    @IBAction func clickYellow(_ sender: UIButton) {
+        simonSaysGame.guessYellow()
+        
+        buttonsClicked += 1
+        results()
+    }
+    
+    @IBAction func clickBlue(_ sender: UIButton) {
+        simonSaysGame.guessBlue()
+        
+        buttonsClicked += 1
+        results()
+    }
+    
+    func results() {
+        if buttonsClicked == simonSaysGame.numberOfColorsToMatch {
+            if simonSaysGame.wonGame() {
+                winLabel.text = "You Won!"
+            } else {
+                winLabel.text = "You Lost"
+            }
+            winLabel.isHidden = false
+            startGameButton.isHidden = false
+            buttonsClicked = 0
+        }
+    }
+    
 }
 
 // MARK: - SimonSays Game Methods
