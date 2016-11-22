ca4296043d6e97a2a2c3ea173655f73a477c573d
IT Development <itdevelopment@ITs-Mac-mini.local>
2016-11-22 09:57:46 +0000

    Lab Completed

--- SimonSaysLab/ViewController.swift
+++ SimonSaysLab/ViewController.swift
@@ -13,11 +13,58 @@ class ViewController: UIViewController {
     @IBOutlet weak var displayColorView: UIView!
     @IBOutlet weak var startGameButton: UIButton!
     @IBOutlet weak var winLabel: UILabel!
+    
+    
+    @IBOutlet weak var buttonRed: UIButton!
+    @IBOutlet weak var buttonGreen: UIButton!
+    @IBOutlet weak var buttonBlue: UIButton!
+    @IBOutlet weak var buttonYellow: UIButton!
+    
     var simonSaysGame = SimonSays()
     var buttonsClicked = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        winLabel.isHidden = true
+    }
+    
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        buttonsClicked += 1
+        
+        switch sender {
+        case buttonRed:
+            simonSaysGame.guessRed()
+        case buttonGreen:
+            simonSaysGame.guessGreen()
+        case buttonYellow:
+            simonSaysGame.guessYellow()
+        case buttonBlue:
+            simonSaysGame.guessBlue()
+        default:
+            return
+        }
+        
+        checkForWinner()
+    }
+    
+    func checkForWinner()
+    {
+        if buttonsClicked == simonSaysGame.numberOfColorsToMatch {
+            if simonSaysGame.wonGame() {
+                winLabel.text = "Congratulations!"
+            } else {
+                winLabel.text = "Sorry - that's not correct"
+            }
+            
+            simonSaysGame = SimonSays()
+            buttonsClicked = 0
+            startGameButton.isHidden = false
+        }
+        else
+        {
+            winLabel.text = "\(simonSaysGame.numberOfColorsToMatch - buttonsClicked) guesses to go..."
+        }
     }
 }
 
@@ -29,6 +76,9 @@ extension ViewController {
             self.startGameButton.isHidden = true
             }, completion: nil)
         
+        
+        winLabel.isHidden = false
+        winLabel.text = "Choosing Colours..."
         displayTheColors()
     }
     
@@ -44,6 +94,8 @@ extension ViewController {
                 } else {
                     self.view.isUserInteractionEnabled = true
                     print("Pattern to match: \(self.simonSaysGame.patternToMatch)")
+                    
+                    self.checkForWinner()
                 }
         })
     }
