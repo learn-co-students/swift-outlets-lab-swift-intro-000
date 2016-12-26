595d03c0e97271752d959f88402d06c5c381cd46
rlopez178 <rlopez178@yahoo.com>
2016-12-26 01:18:32 -0500

    Lab complete finally

--- SimonSaysLab/ViewController.swift
+++ SimonSaysLab/ViewController.swift
@@ -10,16 +10,79 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    
+    @IBOutlet weak var Red: UIButton!
+    @IBOutlet weak var Green: UIButton!
+    @IBOutlet weak var Yellow: UIButton!
+    @IBOutlet weak var Blue: UIButton!
+    
+    
     @IBOutlet weak var displayColorView: UIView!
     @IBOutlet weak var startGameButton: UIButton!
     @IBOutlet weak var winLabel: UILabel!
     var simonSaysGame = SimonSays()
     var buttonsClicked = 0
     
+    
     override func viewDidLoad() {
+        
+        winLabel.isHidden = true
+        
         super.viewDidLoad()
     }
-}
+    
+    
+    func Gameresult() {
+        if (simonSaysGame.chosenColors == simonSaysGame.patternToMatch) {
+            if simonSaysGame.wonGame() {
+                winLabel.text = "Hurray you've won!"
+                winLabel.isHidden = false
+                startGameButton.isHidden = false
+            }
+                
+            else {
+                winLabel.text = "Ya lost Buddy try again"
+                winLabel.isHidden = false
+                startGameButton.isHidden = false
+                
+            }
+            
+            simonSaysGame = SimonSays()
+            winLabel.isHidden = false
+            buttonsClicked = 0
+            
+        }
+        else {
+            winLabel.text = "Hey bro, only " + String(simonSaysGame.patternToMatch.count - simonSaysGame.chosenColors.count) + " to go"
+            winLabel.isHidden = false
+        }
+    }
+    
+    @IBAction func CInputs(_ sender: UIButton) {
+        buttonsClicked = +1
+    
+        switch sender{
+        case Red: simonSaysGame.guessRed()
+        
+        case Blue: simonSaysGame.guessBlue()
+        
+        case Yellow: simonSaysGame.guessYellow()
+        
+        case Green: simonSaysGame.guessGreen()
+        
+        default: return
+        }
+        Gameresult()
+    }
+    
+    
+    }
+
+
+
+
+    
 
 // MARK: - SimonSays Game Methods
 extension ViewController {
@@ -30,6 +93,8 @@ extension ViewController {
             }, completion: nil)
         
         displayTheColors()
+        winLabel.isHidden = true
+        buttonsClicked = 0
     }
     
     fileprivate func displayTheColors() {
