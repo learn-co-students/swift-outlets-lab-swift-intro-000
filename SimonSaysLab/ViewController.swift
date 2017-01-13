c445c99eae08cfa0c0aae5f0bed7fd436b2678f0
kj <twelvejapan@gmail.com>
2017-01-13 16:32:14 -0500

    simon says

--- SimonSaysLab/ViewController.swift
+++ SimonSaysLab/ViewController.swift
@@ -16,15 +16,80 @@ class ViewController: UIViewController {
     var simonSaysGame = SimonSays()
     var buttonsClicked = 0
     
+    // show the result of the game to user
+    func checkResult() {
+        if buttonsClicked == simonSaysGame.patternToMatch.count {
+            if simonSaysGame.wonGame() == true {
+                // user won
+                winLabel.text = "You won!"
+            } else {
+                // user lost
+                winLabel.text = "Nope, try again."
+            }
+            winLabel.isHidden = false
+            
+            // display restart button title: restart
+            startGameButton.isHidden = false
+        }
+    }
+    
+//    func restartGame () {
+//        print("restart game")
+//        buttonsClicked = 0
+//        winLabel.isHidden = true
+//        //simonSaysGame.colorToDisplay = 0
+//        self.startGameTapped(startGameButton)
+//    
+//    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // hide winLabel
+        winLabel.isHidden = true
     }
+    
+    // tap red btn
+    @IBAction func tapRed(_ sender: Any) {
+        print("red was tapped")
+        simonSaysGame.guessRed()
+        buttonsClicked+=1
+        checkResult()
+    }
+    
+    // tap green btn
+    @IBAction func tapGreen(_ sender: Any) {
+        print("green was tapped")
+        simonSaysGame.guessGreen()
+        buttonsClicked+=1
+        checkResult()
+    }
+    
+    // tap yellow btn
+    @IBAction func tapYellow(_ sender: Any) {
+        print("yellow was tapped")
+        simonSaysGame.guessYellow()
+        buttonsClicked+=1
+        checkResult()
+    }
+    
+    // tap blue btn
+    @IBAction func tapBlue(_ sender: Any) {
+        print("blue was tapped")
+        simonSaysGame.guessBlue()
+        buttonsClicked+=1
+        checkResult()
+    }
+    
+    
+    
 }
 
 // MARK: - SimonSays Game Methods
 extension ViewController {
     
     @IBAction func startGameTapped(_ sender: UIButton) {
+        print("startGameTapped called")
         UIView.transition(with: startGameButton, duration: 0.9, options: .transitionFlipFromBottom , animations: {
             self.startGameButton.isHidden = true
             }, completion: nil)
