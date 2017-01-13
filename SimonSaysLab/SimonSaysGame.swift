cf910a7c804ccbb7334193df0033cb26b7f1f61a
Ryan Hennings <r.hennings11@gmail.com>
2017-01-13 01:43:32 -0800

    Simon Says Game

--- SimonSaysLab/SimonSaysGame.swift
+++ SimonSaysLab/SimonSaysGame.swift
@@ -52,7 +52,10 @@ struct SimonSays {
     var chosenColors = [Color]()
     var patternToMatch = [Color]()
     var colorToDisplay = 0
+    var gameRepeat = true
+    var gameNumber = 1
     let numberOfColorsToMatch: Int
+    
     
     init(numberOfColorsToMatch: Int = 5) {
         self.numberOfColorsToMatch = numberOfColorsToMatch
@@ -68,7 +71,14 @@ struct SimonSays {
 
 // MARK: - Gameplay methods
 extension SimonSays {
-
+    
+    mutating func resetColors(numberOfColorsToMatch: Int = 5) {
+        for _ in (0..<numberOfColorsToMatch) {
+            let randomNumber = Int(arc4random_uniform(4))
+            let randomColor = Color(rawValue: randomNumber)!
+            patternToMatch.append(randomColor)
+        }
+    }
     mutating func nextColor() -> Color? {
         var color: Color? = nil
         if colorToDisplay < patternToMatch.count {
@@ -108,8 +118,10 @@ extension SimonSays {
         makeGuessWith(.blue)
     }
     
-    mutating func tryAgainWithTheSamePattern() {
-        chosenColors.removeLast()
-        // display the colors in order again to the user (up to the turn)
+    mutating func tryAgainWithTheSamePattern(numberOfColorsToMatch: Int = 5, _ pattern: [Color]) {
+        patternToMatch.removeAll()
+        for i in (0..<numberOfColorsToMatch) {
+            patternToMatch.append(pattern[i])
+        }
     }
 }
--- SimonSaysLab/ViewController.swift
+++ SimonSaysLab/ViewController.swift
@@ -13,11 +13,99 @@ class ViewController: UIViewController {
     @IBOutlet weak var displayColorView: UIView!
     @IBOutlet weak var startGameButton: UIButton!
     @IBOutlet weak var winLabel: UILabel!
+    
+    @IBOutlet weak var redButton: UIButton!
+    @IBOutlet weak var greenButton: UIButton!
+    @IBOutlet weak var yelloButton: UIButton!
+    @IBOutlet weak var blueButton: UIButton!
+    
     var simonSaysGame = SimonSays()
+    var userPicks = [Color]()
     var buttonsClicked = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        winLabel.text = ""
+        
+        blueButton.backgroundColor = UIColor.blue
+        redButton.backgroundColor = UIColor.red
+        yelloButton.backgroundColor = UIColor.yellow
+        greenButton.backgroundColor = UIColor.green
+        
+        showColorButtons(alphaValue: 0.0)
+    }
+    
+    func showColorButtons(alphaValue: CGFloat) {
+        blueButton.alpha = alphaValue
+        redButton.alpha = alphaValue
+        yelloButton.alpha = alphaValue
+        greenButton.alpha = alphaValue
+    }
+    
+    func wait(seconds: Int, showAlpha: CGFloat, restart: Bool) {
+        unowned let unownedSelf = self
+        let deadlineTime = DispatchTime.now() + .seconds(seconds)
+        DispatchQueue.main.asyncAfter(deadline: deadlineTime, execute: {
+            unownedSelf.showColorButtons(alphaValue: showAlpha)
+            if restart {
+                self.startGameButton.isHidden = false
+            }
+        })
+    }
+    
+    func gameProgress(_ count: Int) {
+        if simonSaysGame.wonGame(){
+            winLabel.text = "You got lucky this time..."
+            restartGame(didWin: true)
+        } else if count == 5 && !simonSaysGame.wonGame() {
+            winLabel.text = "Nope, try again nerd."
+            restartGame(didWin: false)
+        }
+    }
+    
+    func restartGame(didWin: Bool) {
+        print("(\(self.simonSaysGame.gameNumber)) User chose pattern: \(self.simonSaysGame.chosenColors)")
+        wait(seconds: 1, showAlpha: 0.0, restart: true)
+        userPicks.removeAll()
+        pressCount = 0
+        simonSaysGame.colorToDisplay = 0
+        simonSaysGame.chosenColors.removeAll()
+        
+        if didWin {
+            startGameButton.setTitle("Play again?",for: .normal)
+            simonSaysGame.gameNumber += 1
+            simonSaysGame.gameRepeat = false
+            simonSaysGame.patternToMatch.removeAll()
+        } else {
+            startGameButton.setTitle("Try again?",for: .normal)
+            simonSaysGame.gameRepeat = true
+            simonSaysGame.tryAgainWithTheSamePattern(simonSaysGame.patternToMatch)
+        }
+    }
+    
+    var pressCount = 0
+    
+    @IBAction func redPressed(_ sender: Any) {
+        simonSaysGame.guessRed()
+        pressCount += 1
+        gameProgress(pressCount)
+    }
+    @IBAction func greenPressed(_ sender: Any) {
+        simonSaysGame.guessGreen()
+        pressCount += 1
+        gameProgress(pressCount)
+    }
+    @IBAction func YellowPressed(_ sender: Any) {
+        simonSaysGame.guessYellow()
+        pressCount += 1
+        gameProgress(pressCount)
+    }
+    @IBAction func bluePressed(_ sender: Any) {
+        simonSaysGame.guessBlue()
+        userPicks.append(.blue)
+        pressCount += 1
+        gameProgress(pressCount)
     }
 }
 
@@ -29,12 +117,18 @@ extension ViewController {
             self.startGameButton.isHidden = true
             }, completion: nil)
         
+        if simonSaysGame.gameRepeat == false {
+            simonSaysGame.resetColors()
+        }
+        
         displayTheColors()
+        winLabel.text = "Playing..."
+        wait(seconds: 8, showAlpha: 1.0, restart: false)
     }
     
     fileprivate func displayTheColors() {
         self.view.isUserInteractionEnabled = false
-        UIView.transition(with: displayColorView, duration: 1.5, options: .transitionCurlUp, animations: {
+        UIView.transition(with: displayColorView, duration: 1.4, options: .transitionCurlUp, animations: {
             self.displayColorView.backgroundColor = self.simonSaysGame.nextColor()?.colorToDisplay
             self.displayColorView.alpha = 0.0
             self.displayColorView.alpha = 1.0
@@ -43,7 +137,7 @@ extension ViewController {
                     self.displayTheColors()
                 } else {
                     self.view.isUserInteractionEnabled = true
-                    print("Pattern to match: \(self.simonSaysGame.patternToMatch)")
+                    print("(\(self.simonSaysGame.gameNumber)) Pattern to match:   \(self.simonSaysGame.patternToMatch)")
                 }
         })
     }
