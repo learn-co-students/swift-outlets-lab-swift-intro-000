f85093dce96797bead36c453fb3d0fd8afac6470
Crystal Mora <cmora427@gmail.com>
2017-01-30 23:34:22 -0500

    completed

--- SimonSaysLab/ViewController.swift
+++ SimonSaysLab/ViewController.swift
@@ -18,6 +18,53 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        winLabel.isHidden = true
+    }
+    
+    @IBAction func presRed(_ sender: Any) {
+        simonSaysGame.guessRed()
+        print( "red pressed")
+        
+       checkGameStatus()
+    }
+    @IBAction func pressBlue(_ sender: Any) {
+        simonSaysGame.guessBlue()
+        print ("blue pressed")
+        
+        checkGameStatus()
+    }
+    @IBAction func pressYellow(_ sender: Any) {
+        simonSaysGame.guessYellow()
+        print ("yellow pressed")
+        
+        checkGameStatus()
+    }
+    
+    @IBAction func pressGreen(_ sender: Any) {
+        simonSaysGame.guessGreen()
+        print ("green pressed")
+        
+        checkGameStatus()
+    }
+    
+    func checkGameStatus() {
+        if simonSaysGame.wonGame() {
+            print("won game")
+            winLabel.text = "You Won!"
+            winLabel.isHidden = false
+            restartGame()
+        }  else if simonSaysGame.chosenColors.count == simonSaysGame.numberOfColorsToMatch {
+            print ("lost game")
+            winLabel.text = "Nope, try again."
+            winLabel.isHidden = false
+            restartGame()
+        }
+    }
+    
+    func restartGame() {
+        simonSaysGame = SimonSays()
+        startGameButton.isHidden = false
+        
     }
 }
 
@@ -28,6 +75,7 @@ extension ViewController {
         UIView.transition(with: startGameButton, duration: 0.9, options: .transitionFlipFromBottom , animations: {
             self.startGameButton.isHidden = true
             }, completion: nil)
+        winLabel.isHidden = true
         
         displayTheColors()
     }
@@ -48,3 +96,4 @@ extension ViewController {
         })
     }
 }
+
