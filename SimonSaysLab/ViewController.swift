e9aa3c98239ed76dc86ea9269716cc41dff9c1ff
John Doe <dssp@freenet.de>
2017-03-24 14:09:33 +0100

    Completed SimonSays Lab

--- SimonSaysLab/ViewController.swift
+++ SimonSaysLab/ViewController.swift
@@ -16,8 +16,65 @@ class ViewController: UIViewController {
     var simonSaysGame = SimonSays()
     var buttonsClicked = 0
     
-    override func viewDidLoad() {
+    @IBAction func redButtonPressed(_ sender: Any)
+    {
+        simonSaysGame.guessRed()
+        buttonsClicked += 1
+        checkResult()
+        print("Red Pressed")
+    }
+    
+    
+    @IBAction func greenButtonPressed(_ sender: Any)
+    {
+        simonSaysGame.guessGreen()
+        buttonsClicked += 1
+        checkResult()
+        print("Green Pressed")
+    }
+    
+    
+    @IBAction func yellowButtonPressed(_ sender: Any)
+    {
+        simonSaysGame.guessYellow()
+        buttonsClicked += 1
+        checkResult()
+        print("Yelllow Pressed")
+    }
+    
+    
+    @IBAction func blueButtonPressed(_ sender: Any)
+    {
+        simonSaysGame.guessBlue()
+        buttonsClicked += 1
+        checkResult()
+        print("Blue Pressed")
+    }
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        winLabel.isHidden = true
+    }
+    
+    func checkResult(){
+        
+        if buttonsClicked == 5
+        {
+            if simonSaysGame.patternToMatch == simonSaysGame.chosenColors
+            {
+                print("You Won")
+                winLabel.text = "You Won"
+                winLabel.isHidden = false
+            }
+            else
+            {
+                print("You Lost")
+                winLabel.text = "You Lost"
+                winLabel.isHidden = false
+            }
+        }
     }
 }
 
@@ -27,7 +84,7 @@ extension ViewController {
     @IBAction func startGameTapped(_ sender: UIButton) {
         UIView.transition(with: startGameButton, duration: 0.9, options: .transitionFlipFromBottom , animations: {
             self.startGameButton.isHidden = true
-            }, completion: nil)
+        }, completion: nil)
         
         displayTheColors()
     }
@@ -38,13 +95,17 @@ extension ViewController {
             self.displayColorView.backgroundColor = self.simonSaysGame.nextColor()?.colorToDisplay
             self.displayColorView.alpha = 0.0
             self.displayColorView.alpha = 1.0
-            }, completion: { _ in
-                if !self.simonSaysGame.sequenceFinished() {
-                    self.displayTheColors()
-                } else {
-                    self.view.isUserInteractionEnabled = true
-                    print("Pattern to match: \(self.simonSaysGame.patternToMatch)")
-                }
+        }, completion: { _ in
+            if !self.simonSaysGame.sequenceFinished() {
+                self.displayTheColors()
+            } else {
+                self.view.isUserInteractionEnabled = true
+                print("Pattern to match: \(self.simonSaysGame.patternToMatch)")
+            }
         })
     }
+    
+    
+    
+    
 }
