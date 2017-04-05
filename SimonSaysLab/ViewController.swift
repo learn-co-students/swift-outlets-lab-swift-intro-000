686f336d67933763e710eb3eeb4f67d53d7ab3d1
Ubada2000 <ubada@digicallconnect.com>
2017-04-05 03:17:33 +0200

    Complete problems

--- SimonSaysLab/ViewController.swift
+++ SimonSaysLab/ViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
+    
     
     @IBOutlet weak var displayColorView: UIView!
     @IBOutlet weak var startGameButton: UIButton!
     @IBOutlet weak var winLabel: UILabel!
     var simonSaysGame = SimonSays()
     var buttonsClicked = 0
+    
+    
+    @IBAction func redButton(_ sender: Any) {
+        simonSaysGame.guessRed()
+        buttonsClicked += 1
+        endGame()
+    }
+    @IBAction func greenButton(_ sender: Any) {
+        simonSaysGame.guessGreen()
+        buttonsClicked += 1
+        endGame()
+    }
+    @IBAction func yellowButton(_ sender: Any) {
+        simonSaysGame.guessYellow()
+        buttonsClicked += 1
+        endGame()
+    }
+    @IBAction func blueButton(_ sender: Any) {
+        simonSaysGame.guessBlue()
+        buttonsClicked += 1
+        endGame()
+    }
+    func endGame() {
+        if buttonsClicked == simonSaysGame.patternToMatch.count && simonSaysGame.wonGame() {
+            winLabel.isHidden = false
+            winLabel.text = "You Won!!!"
+            print("Chosen Colors: \(self.simonSaysGame.chosenColors)")
+        } else if buttonsClicked == simonSaysGame.patternToMatch.count && simonSaysGame.wonGame() == false {
+            winLabel.isHidden = false
+            winLabel.text = "you Lost :("
+            print("Chosen Colors: \(self.simonSaysGame.chosenColors)")
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
