e4d2767fb5114414204c487e138ecd14095def08
Ronak Bansal <ronakb@berkeley.edu>
2017-06-02 12:11:57 +0530

    simon says

--- SimonSaysLab/ViewController.swift
+++ SimonSaysLab/ViewController.swift
@@ -13,11 +13,52 @@ class ViewController: UIViewController {
     @IBOutlet weak var displayColorView: UIView!
     @IBOutlet weak var startGameButton: UIButton!
     @IBOutlet weak var winLabel: UILabel!
+    var guess = 0
+    func check(val : Int) {
+        if guess == 5{
+            if simonSaysGame.wonGame() {
+                winLabel.isHidden = false
+                winLabel.text = "You won!"
+                
+            }
+            else {
+                winLabel.isHidden = false
+                winLabel.text = "Nope, try again."
+            }
+        }
+        
+    }
+    @IBAction func red1(_ sender: Any) {
+        simonSaysGame.guessRed()
+        guess+=1
+        check(val:guess)
+    }
+
+    @IBAction func green1(_ sender: Any) {
+                simonSaysGame.guessGreen()
+                guess+=1
+                check(val:guess)
+    }
+
+    @IBAction func yellow1(_ sender: Any) {
+                simonSaysGame.guessYellow()
+                guess+=1
+                check(val:guess)
+    }
+
+    @IBAction func blue1(_ sender: Any) {
+                simonSaysGame.guessBlue()
+                guess+=1
+                check(val:guess)
+    }
+
+    
     var simonSaysGame = SimonSays()
     var buttonsClicked = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        winLabel.isHidden = true
     }
 }
 
