6a7d290eb08210dc15da77a3b6d98302469e202c
Garett Arrowood <garettarrowood@gmail.com>
2017-06-03 10:50:12 -0400

    Add IBActions for new buttons

--- SimonSaysLab/ViewController.swift
+++ SimonSaysLab/ViewController.swift
@@ -1,23 +1,29 @@
-//
-//  ViewController.swift
-//  SimonSaysLab
-//
-//  Created by James Campagno on 5/31/16.
-//  Copyright © 2016 Flatiron School. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-    
     @IBOutlet weak var displayColorView: UIView!
     @IBOutlet weak var startGameButton: UIButton!
     @IBOutlet weak var winLabel: UILabel!
+
+    @IBAction func redButton(_ sender: Any) {
+        simonSaysGame.guessRed()
+    }
+    @IBAction func greenButton(_ sender: Any) {
+        simonSaysGame.guessGreen()
+    }
+    @IBAction func yellowButton(_ sender: Any) {
+        simonSaysGame.guessYellow()
+    }
+    @IBAction func blueButton(_ sender: Any) {
+        simonSaysGame.guessBlue()
+    }
+
     var simonSaysGame = SimonSays()
     var buttonsClicked = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        winLabel.isHidden = true
     }
 }
 
