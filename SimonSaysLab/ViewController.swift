67755d70e9f1b8e51e4694ff3d11b85e8c5244f0
chelleccarlyle <chelle_carlyle@yahoo.com>
2017-07-06 22:41:24 -0700

    Implement Simon Says app
    Allows player to play again whether they win or lose
    
    Add animations for buttons to flash whenever they are pressed

--- SimonSaysLab/ViewController.swift
+++ SimonSaysLab/ViewController.swift
@@ -13,12 +13,85 @@ class ViewController: UIViewController {
     @IBOutlet weak var displayColorView: UIView!
     @IBOutlet weak var startGameButton: UIButton!
     @IBOutlet weak var winLabel: UILabel!
+    @IBOutlet weak var redButton: UIButton!
+    @IBOutlet weak var greenButton: UIButton!
+    @IBOutlet weak var yellowButton: UIButton!
+    @IBOutlet weak var blueButton: UIButton!
+    
+    
     var simonSaysGame = SimonSays()
     var buttonsClicked = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        winLabel.isHidden = true
+        //Make buttons uncliackable except start game button
+        redButton.isEnabled = false
+        greenButton.isEnabled = false
+        yellowButton.isEnabled = false
+        blueButton.isEnabled = false
     }
+    
+    @IBAction func pressedRed(_ sender: Any) {
+        
+        UIView.animate(withDuration: 0.2, delay: 0, options: UIViewAnimationOptions.curveEaseOut, animations: {
+            
+            self.redButton.alpha = 0.6
+            self.redButton.alpha = 1.0
+            
+        }, completion: nil)
+        
+        print("Pressed red")
+        simonSaysGame.guessRed()
+        buttonsClicked += 1
+        checkResult()
+    }
+    @IBAction func pressedGreen(_ sender: Any) {
+        
+        UIView.animate(withDuration: 0.2, delay: 0, options: UIViewAnimationOptions.curveEaseOut, animations: {
+            
+            self.greenButton.alpha = 0.6
+            self.greenButton.alpha = 1.0
+            
+        }, completion: nil)
+
+        
+        print("Pressed green")
+        simonSaysGame.guessGreen()
+        buttonsClicked += 1
+        checkResult()
+    }
+    @IBAction func pressedYellow(_ sender: Any) {
+        
+        UIView.animate(withDuration: 0.2, delay: 0, options: UIViewAnimationOptions.curveEaseOut, animations: {
+            
+            self.yellowButton.alpha = 0.6
+            self.yellowButton.alpha = 1.0
+            
+        }, completion: nil)
+
+        
+        print("Pressed yellow")
+        simonSaysGame.guessYellow()
+        buttonsClicked += 1
+        checkResult()
+    }
+    @IBAction func pressedBlue(_ sender: Any) {
+        
+        UIView.animate(withDuration: 0.2, delay: 0, options: UIViewAnimationOptions.curveEaseOut, animations: {
+            
+            self.blueButton.alpha = 0.6
+            self.blueButton.alpha = 1.0
+            
+        }, completion: nil)
+
+        
+        print("Pressed blue")
+        simonSaysGame.guessBlue()
+        buttonsClicked += 1
+        checkResult()
+    }
+    
 }
 
 // MARK: - SimonSays Game Methods
@@ -29,6 +102,7 @@ extension ViewController {
             self.startGameButton.isHidden = true
             }, completion: nil)
         
+        restart()
         displayTheColors()
     }
     
@@ -44,7 +118,60 @@ extension ViewController {
                 } else {
                     self.view.isUserInteractionEnabled = true
                     print("Pattern to match: \(self.simonSaysGame.patternToMatch)")
+                    //Make buttons clickable
+                    self.redButton.isEnabled = true
+                    self.greenButton.isEnabled = true
+                    self.yellowButton.isEnabled = true
+                    self.blueButton.isEnabled = true
                 }
         })
     }
+    
+    func checkResult() {
+        
+        //Check if 5 buttons are clicked, if yes then game is over
+        if (self.buttonsClicked == self.simonSaysGame.patternToMatch.count) {
+            
+            print("Game is over")
+            
+            makeButtonsUnclickable()
+            
+            //Check if player won the game
+            if (self.simonSaysGame.wonGame()) {
+                print("You won")
+                self.winLabel.text = "You won!"
+                self.startGameButton.setTitle("Play Again", for: .normal) //Change start game button text
+            }
+            else {
+                print("You lost")
+                self.winLabel.text = "You lost!"
+                self.startGameButton.setTitle("Try Again", for: .normal) //Change start game button text
+            }
+            
+            self.winLabel.isHidden = false
+            self.startGameButton.isHidden = false
+            
+        }
+        
+    }
+    
+    func restart() {
+        
+        self.simonSaysGame = SimonSays()
+        self.buttonsClicked = 0
+        self.winLabel.isHidden = true
+        
+    }
+    
+    func makeButtonsUnclickable() {
+        
+        //Make buttons uncliackable except start game button
+        self.redButton.isEnabled = false
+        self.greenButton.isEnabled = false
+        self.yellowButton.isEnabled = false
+        self.blueButton.isEnabled = false
+        
+    }
+    
+    
 }
