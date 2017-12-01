750f12312ec1d0ce82b97dc3fc88aad92b05a5a3
Luis Fernando Gutiérrez Romo <changoluis@hotmail.com>
2017-11-30 23:53:02 -0600

    Finished

--- SimonSaysLab/ViewController.swift
+++ SimonSaysLab/ViewController.swift
@@ -13,23 +13,66 @@ class ViewController: UIViewController {
     @IBOutlet weak var displayColorView: UIView!
     @IBOutlet weak var startGameButton: UIButton!
     @IBOutlet weak var winLabel: UILabel!
+
     var simonSaysGame = SimonSays()
     var buttonsClicked = 0
+
+    @IBAction func greenBtn(_ sender: Any) {
+        simonSaysGame.guessGreen()
+        print("greenBtn")
+        compareColors()
+    }
+    @IBAction func redBtn(_ sender: Any) {
+        simonSaysGame.guessRed()
+        print("redBtn")
+        compareColors()
+    }
+    @IBAction func blueBtn(_ sender: Any) {
+        simonSaysGame.guessBlue()
+        print("blueBtn")
+        compareColors()
+    }
+
+    @IBAction func yellowBtn(_ sender: Any) {
+        print("yellowBtn")
+        simonSaysGame.guessYellow()
+        compareColors()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        winLabel.isHidden = true
     }
 }
 
 // MARK: - SimonSays Game Methods
 extension ViewController {
-    
+
     @IBAction func startGameTapped(_ sender: UIButton) {
         UIView.transition(with: startGameButton, duration: 0.9, options: .transitionFlipFromBottom , animations: {
-            self.startGameButton.isHidden = true
             }, completion: nil)
-        
+        winLabel.isHidden = false
         displayTheColors()
+//        showAll()
+        self.startGameButton.isHidden = true
+    }
+
+    func compareColors(){
+        buttonsClicked += 1
+
+        if(simonSaysGame.numberOfColorsToMatch == buttonsClicked){ //compare te timpes the buttn is clicked vs the number of colors
+            if(simonSaysGame.wonGame()){
+                winLabel.isHidden = false //show label
+                winLabel.text = "🎊🎉 You Won 🎉🎊"
+                startGameButton.isHidden = false
+            }
+//            }else{
+//                winLabel.isHidden = false //show label
+//                winLabel.text = "You lose"
+//            }
+//            startGameButton.isHidden = false
+//            winLabel.isHidden = true
+        }
     }
     
     fileprivate func displayTheColors() {
