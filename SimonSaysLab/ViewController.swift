ddc0e23a1762f2a9914afeb6d08be2fce30ea640
flanding001 <frederik@landing.se>
2018-02-18 20:53:10 +0100

    Complete problems

--- SimonSaysLab/ViewController.swift
+++ SimonSaysLab/ViewController.swift
@@ -16,10 +16,47 @@ class ViewController: UIViewController {
     var simonSaysGame = SimonSays()
     var buttonsClicked = 0
     
+// color buttons
+    @IBAction func btRed(_ sender: Any) {
+        simonSaysGame.guessRed()
+        buttonsClicked = buttonsClicked + 1
+        if simonSaysGame.numberOfColorsToMatch == buttonsClicked{
+            checkIfWon()
+        }
+    }
+    
+    @IBAction func btGreen(_ sender: Any) {
+        simonSaysGame.guessGreen()
+        buttonsClicked = buttonsClicked + 1
+        if simonSaysGame.numberOfColorsToMatch == buttonsClicked{
+            checkIfWon()
+        }
+    }
+    
+    @IBAction func btYellow(_ sender: Any) {
+        simonSaysGame.guessYellow()
+        buttonsClicked = buttonsClicked + 1
+        if simonSaysGame.numberOfColorsToMatch == buttonsClicked{
+            checkIfWon()
+        }
+    }
+    
+    @IBAction func btBlue(_ sender: Any) {
+        simonSaysGame.guessBlue()
+        buttonsClicked = buttonsClicked + 1
+        if simonSaysGame.numberOfColorsToMatch == buttonsClicked{
+            checkIfWon()
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.winLabel.isHidden = true
     }
 }
+
+
 
 // MARK: - SimonSays Game Methods
 extension ViewController {
@@ -30,6 +67,7 @@ extension ViewController {
             }, completion: nil)
         
         displayTheColors()
+
     }
     
     fileprivate func displayTheColors() {
@@ -44,7 +82,20 @@ extension ViewController {
                 } else {
                     self.view.isUserInteractionEnabled = true
                     print("Pattern to match: \(self.simonSaysGame.patternToMatch)")
+
                 }
         })
     }
+
+    func checkIfWon(){
+        if self.simonSaysGame.wonGame() {
+            self.winLabel.text = "Du vann"
+            self.winLabel.isHidden = false
+            
+        } else {
+            self.winLabel.text = "Du förlorade ... "
+            self.winLabel.isHidden = false
+        }
+    }
+
 }
