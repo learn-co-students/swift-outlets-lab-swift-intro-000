7e9bb31abf32bbb4054a97a5cd9e876205d5a607
“dnomyar105” <dnomyar105@yahoo.com>
2018-11-28 15:42:30 -0500

    Complete problems

--- SimonSaysLab/ViewController.swift
+++ SimonSaysLab/ViewController.swift
@@ -15,9 +15,59 @@ class ViewController: UIViewController {
     @IBOutlet weak var winLabel: UILabel!
     var simonSaysGame = SimonSays()
     var buttonsClicked = 0
+    @IBAction func buttonRed(_ sender: Any) {
+        simonSaysGame.guessRed()
+        buttonsClicked += 1
+        checkWin(buttonClicks: buttonsClicked)
+    }
+    @IBAction func buttonGreen(_ sender: Any) {
+        simonSaysGame.guessGreen()
+        buttonsClicked += 1
+        checkWin(buttonClicks: buttonsClicked)
+    }
+    @IBAction func buttonYellow(_ sender: Any) {
+        simonSaysGame.guessYellow()
+        buttonsClicked += 1
+        checkWin(buttonClicks: buttonsClicked)
+    }
+    @IBAction func buttonBlue(_ sender: Any) {
+        simonSaysGame.guessBlue()
+        buttonsClicked += 1
+        checkWin(buttonClicks: buttonsClicked)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        winLabel.isHidden = true
+    }
+    
+    func checkWin(buttonClicks: Int) {
+        if buttonClicks == 5 {
+            if simonSaysGame.wonGame() == true {
+                winLabel.text = "You Won!"
+                winLabel.isHidden = false
+                startGameButton.isHidden = false
+                buttonsClicked = 0
+            } else {
+                winLabel.text = "You Lose!"
+                winLabel.isHidden = false
+                startGameButton.isHidden = false
+                buttonsClicked = 0
+            }
+        }
+    }
+    
+    func resetSequence() {
+        simonSaysGame.patternToMatch.removeAll()
+        simonSaysGame.colorToDisplay = 0
+        simonSaysGame.chosenColors.removeAll()
+        winLabel.text = "Playing"
+        
+        for _ in (0..<simonSaysGame.numberOfColorsToMatch) {
+            let randomNumber = Int(arc4random_uniform(4))
+            let randomColor = Color(rawValue: randomNumber)!
+            simonSaysGame.patternToMatch.append(randomColor)
+        }
     }
 }
 
@@ -25,14 +75,15 @@ class ViewController: UIViewController {
 extension ViewController {
     
     @IBAction func startGameTapped(_ sender: UIButton) {
+        self.resetSequence()
         UIView.transition(with: startGameButton, duration: 0.9, options: .transitionFlipFromBottom , animations: {
             self.startGameButton.isHidden = true
             }, completion: nil)
-        
         displayTheColors()
     }
     
     fileprivate func displayTheColors() {
+        self.winLabel.isHidden = false
         self.view.isUserInteractionEnabled = false
         UIView.transition(with: displayColorView, duration: 1.5, options: .transitionCurlUp, animations: {
             self.displayColorView.backgroundColor = self.simonSaysGame.nextColor()?.colorToDisplay
@@ -44,6 +95,7 @@ extension ViewController {
                 } else {
                     self.view.isUserInteractionEnabled = true
                     print("Pattern to match: \(self.simonSaysGame.patternToMatch)")
+                    self.winLabel.text = "Your turn!"
                 }
         })
     }
