6fa7636b91feb471bb4080c53992391c459dcfee
Alicia MacCara <aliciamaccara@gmail.com>
2019-01-04 10:28:36 -0500

    assignment completed

--- SimonSaysLab/ViewController.swift
+++ SimonSaysLab/ViewController.swift
@@ -16,6 +16,61 @@ class ViewController: UIViewController {
     var simonSaysGame = SimonSays()
     var buttonsClicked = 0
     
+    @IBAction func redButton(_ sender: UIButton) {
+        simonSaysGame.guessRed()
+        buttonsClicked += 1
+        print("red clicked" , buttonsClicked)
+        count(clicks:buttonsClicked)
+    }
+    
+    @IBAction func greenButton(_ sender: UIButton) {
+        simonSaysGame.guessGreen()
+        buttonsClicked += 1
+        print("green clicked", buttonsClicked)
+        count(clicks:buttonsClicked)
+        
+    }
+    
+    @IBAction func yellowButton(_ sender: UIButton) {
+        simonSaysGame.guessYellow()
+        buttonsClicked += 1
+        print("yellow clicked", buttonsClicked)
+        count(clicks:buttonsClicked)
+    }
+    
+    @IBAction func blueButton(_ sender: UIButton) {
+        simonSaysGame.guessBlue()
+        buttonsClicked += 1
+        print("blue clicked", buttonsClicked)
+        count(clicks:buttonsClicked)
+        
+    }
+    
+    func count(clicks: Int){
+        let clicks = buttonsClicked
+        if(clicks == 5){
+            if(simonSaysGame.wonGame())
+            {
+                winLabel.isHidden = false
+                winLabel.text = "You Won!"
+            }
+            else
+            {
+                winLabel.isHidden = false
+                winLabel.text = "Try again"
+            }
+            
+        }
+        else if(clicks == 6){
+            buttonsClicked = 0;
+            startGameButton.isHidden = false
+            winLabel.isHidden = true
+        }
+        else{
+            
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -46,5 +101,7 @@ extension ViewController {
                     print("Pattern to match: \(self.simonSaysGame.patternToMatch)")
                 }
         })
-    }
+   }
+    
+    
 }
